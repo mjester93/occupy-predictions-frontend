fdf69bc3dee3cf79eac0d801b0b637c517b509ac
mjester93 <m.jester.93@gmail.com>
2020-08-26 11:29:44 -0400

    adding routes to header

--- src/components/Header.js
+++ src/components/Header.js
@@ -12,13 +12,13 @@ const Header = () => {
             <Menu.Item name="My Profile" href="/my-profile" />
             <Menu.Item name='Leaderboard' href='/leaderboard' />
             <Menu.Item name='My Follows' href='/my-follows' />
-            <Menu.Item position='right'>
+            <Menu.Item href="/login" position='right'>
                 <Button className="header-button">Login</Button>
             </Menu.Item>
-            <Menu.Item>
+            <Menu.Item href="/sign-up">
                 <Button className="header-button">Sign Up</Button>
             </Menu.Item>
-            <Menu.Item>
+            <Menu.Item href="logout">
                 <Button className="header-button">Logout</Button>
             </Menu.Item>
         </Menu>
