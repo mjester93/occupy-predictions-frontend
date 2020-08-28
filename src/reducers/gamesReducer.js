5a6397ed64e69f6e6cb25cf9befbdf3b87d26ff6
mjester93 <m.jester.93@gmail.com>
2020-08-28 02:07:44 -0400

    minor styling changes

--- src/reducers/gamesReducer.js
+++ src/reducers/gamesReducer.js
@@ -3,18 +3,10 @@ let initialState = {games: [], loading: false}
 let gamesReducer = (state=initialState, action) => {
   switch(action.type) {
     case 'LOADING_SCHEDULED_GAMES':
-      return {
-        ...state,
-        games: [...state.games],
-        loading: true
-      }
+      return {...state, games: [...state.games], loading: true}
     
     case 'ADD_SCHEDULED_GAMES':
-      return {
-        ...state,
-        games: action.games,
-        loading: false
-      }
+      return {...state, games: action.games, loading: false}
 
     default:
       return state
--- src/reducers/usersReducer.js
+++ src/reducers/usersReducer.js
@@ -1,20 +1,19 @@
-let initialState = {user: {}, loading: false}
+let initialState = {user: {}, loggedIn: false, loading: false}
 
 let usersReducer = (state=initialState, action) => {
   switch(action.type) {
+
+    case 'LOG_USER_IN':
+      return {...state, loggedIn: true}
+    
+    case 'LOG_USER_OUT':
+      return {...state, loggedIn: false}
+
     case 'LOADING_USER_INFORMATION':
-      return {
-        ...state,
-        user: {...state.user},
-        loading: true
-      }
+      return {...state, user: {...state.user}, loading: true}
     
     case 'ADD_USER_INFORMATION':
-      return {
-        ...state,
-        user: action.user,
-        loading: false
-      }
+      return {...state, user: action.user, loading: false}
 
     default:
       return state
