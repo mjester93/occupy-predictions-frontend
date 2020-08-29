cda5854a9732125bd3db4cb0cd8bdf33d6a63828
mjester93 <m.jester.93@gmail.com>
2020-08-29 13:49:20 -0400

    social media handles

--- src/containers/MyProfileLeftSideContainer.js
+++ src/containers/MyProfileLeftSideContainer.js
@@ -9,37 +9,69 @@ const MyProfileLeftSideContainer = (props) => {
     const userNameHeader = () => {
         return (
             <div className="username-with-badges">
-                {/* <h2 className="my-profile-username">{ loading ? 'Username' : user.username}</h2> */}
+                <h2 className="my-profile-username">{ loading ? 'Username' : user.username}</h2>
                 &nbsp;
-                <Icon circular name="check" id="fab-check" />
+                { user['is_featured'] ? <Icon circular name="check" id="fab-check" /> : null }
                 &nbsp;
-                <Icon id="fab-op" />
+                { user['is_staff'] ? <Icon id="fab-op" /> : null }
             </div>
         )
+    }
+
+    const getTwitterHandle = () => {
+        if (user['twitter_handle']) {
+            return (
+                <a target="_blank" rel="noopener noreferrer" href={`https://www.twitter.com/${user['twitter_handle']}`}>
+                    <Icon link circular name='twitter' id="fab-twitter" />
+                </a>
+            )
+        }
+    }
+
+    const getInstagramHandle = () => {
+        if (user['instagram_handle']) {
+            return (
+                <a target="_blank" rel="noopener noreferrer" href="https://isntagram.com/">
+                    <Icon link circular name='instagram' id="fab-instagram" />
+                </a>
+            )
+        }
+    }
+
+    const getYoutubeHandle = () => {
+        if (user['youtube_handle']) {
+            return (
+                <a target="_blank" rel="noopener noreferrer" href="https://youtube.com/">
+                    <Icon link circular name='youtube play' id='fab-youtube-play' />
+                </a>
+            )
+        }
+    }
+
+    const getTwitchHandle = () => {
+        if (user['twitch_handle']) {
+            return (
+                <a target="_blank" rel="noopener noreferrer" href="https://twitch.tv/">
+                    <Icon link circular name='twitch' id="fab-twitch" />
+                </a>
+            )
+        }
     }
 
     return (
         <Segment style={{border: '1px solid black'}}>
             {userNameHeader()}
             <p>Edit your profile</p>
-            <img alt="avatar" src="https://upload.wikimedia.org/wikipedia/commons/8/89/Portrait_Placeholder.png" width="100px" />
+            <img alt="avatar" src={user.photo} style={{borderRadius: '50%'}} width="100px" />
             <p>115 followers</p>
             <h4>Bio</h4>
             <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Mauris quis tellus a justo finibus auctor.</p>
             <h4>Social Media</h4>
             <div className="social-media-icons">
-                <a target="_blank" rel="noopener noreferrer" href="https://www.twitter.com/">
-                    <Icon link circular name='twitter' id="fab-twitter" />
-                </a>
-                <a target="_blank" rel="noopener noreferrer" href="https://isntagram.com/">
-                    <Icon link circular name='instagram' id="fab-instagram" />
-                </a>
-                <a target="_blank" rel="noopener noreferrer" href="https://youtube.com/">
-                    <Icon link circular name='youtube play' id='fab-youtube-play' />
-                </a>
-                <a target="_blank" rel="noopener noreferrer" href="https://twitch.tv/">
-                    <Icon link circular name='twitch' id="fab-twitch" />
-                </a>
+                {getTwitterHandle()}
+                {getInstagramHandle()}
+                {getYoutubeHandle()}
+                {getTwitchHandle()}
                 <a target="_blank" rel="noopener noreferrer" href="https://reddit.com/u/">
                     <Icon link circular name='reddit alien' id="fab-reddit-alien" />
                 </a>
