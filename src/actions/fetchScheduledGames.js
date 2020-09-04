3d690e0aacfcdc211a4380b7f54c0cdf88d28a38
mjester93 <m.jester.93@gmail.com>
2020-09-04 18:24:03 -0400

    added second fetch for all

--- src/actions/fetchScheduledGames.js
+++ src/actions/fetchScheduledGames.js
@@ -2,7 +2,11 @@ const fetchScheduledGames = () => {
     return (dispatch) => {
         dispatch( {type: 'LOADING_SCHEDULED_GAMES'} )
 
-        fetch('http://localhost:3000/games')
+        fetch('http://localhost:3000/games?limit=5')
+        .then(response => response.json())
+        .then(games => dispatch( {type: 'ADD_SCHEDULED_GAMES', games: games} ))
+
+        fetch('http://localhost:3000/games?limit=999')
         .then(response => response.json())
         .then(games => dispatch( {type: 'ADD_SCHEDULED_GAMES', games: games} ))
     }
